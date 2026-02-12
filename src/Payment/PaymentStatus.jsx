9b208f665b67032394a2f5613ce4ae0a567d792b
PramodKush2810 <pramodkushwaha@techgropse.com>
2026-02-12 12:15:08 +0530

    comment payement status

--- src/Payment/PaymentStatus.jsx
+++ src/Payment/PaymentStatus.jsx
@@ -7,35 +7,35 @@ const PaymentStatus = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const verifyPayment = async () => {
-      const id = searchParams.get("id");
+  // useEffect(() => {
+  //   const verifyPayment = async () => {
+  //     const id = searchParams.get("id");
 
-      if (!id) {
-        navigate("/payment-failed");
-        return;
-      }
+  //     if (!id) {
+  //       navigate("/payment-failed");
+  //       return;
+  //     }
 
-      try {
-        const res = await axios.post(
-          `${process.env.REACT_APP_API}/api/user/checkPaymentStatus`,
-          {
-            id,
-          },
-        );
+  //     try {
+  //       const res = await axios.post(
+  //         `${process.env.REACT_APP_API}/api/user/checkPaymentStatus`,
+  //         {
+  //           id,
+  //         },
+  //       );
 
-        if (res?.data?.success) {
-          navigate("/payment-success");
-        } else {
-          navigate("/payment-failed");
-        }
-      } catch {
-        navigate("/payment-failed");
-      }
-    };
+  //       if (res?.data?.success) {
+  //         navigate("/payment-success");
+  //       } else {
+  //         navigate("/payment-failed");
+  //       }
+  //     } catch {
+  //       navigate("/payment-failed");
+  //     }
+  //   };
 
-    verifyPayment();
-  }, []);
+  //   verifyPayment();
+  // }, []);
 
   return (
     <div className="vh-100 d-flex justify-content-center align-items-center">
